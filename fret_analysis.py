e9cff56f56a56403028967b4d969e6ab25d09deb
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-05-22 23:34:03 +0100

    Compare multiple datasets in analysis
    Nested dicts are a little awkward to work with. Maybe time to consider SQL?

--- fret_analysis.py
+++ fret_analysis.py
@@ -2,7 +2,7 @@
 from itertools import chain
 from functools import partial
 from numpy import amax, amin, sum as asum, mean, std, percentile, clip, linspace
-from collections import OrderedDict
+from collections import defaultdict
 
 def loadAll(f):
     from pickle import load
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     import csv
 
     parser = ArgumentParser(description="Analyze single-particle traces.")
-    parser.add_argument("rois", nargs='+', type=Path,
+    parser.add_argument("--experiment", nargs='+', type=str, action="append",
                         help="The pickled ROIs to process")
     parser.add_argument("--threshold", type=float, default=2.0,
                         help="The fold-increase over background necessary for a spot to be on.")
@@ -41,66 +41,78 @@ if __name__ == "__main__":
     # Must be imported after backend is set
     import matplotlib.pyplot as plt
 
-    rois = []
-    for roi_path in args.rois:
-        with roi_path.open("rb") as f:
-            rois.append(list(loadAll(f)))
-    traces = list(map(list, map(partial(map, partial(amax, axis=(1, 2))), rois)))
-    nrois = sum(map(len, rois))
+    experiments = defaultdict(list)
+    for experiment in args.experiment:
+        name = experiment[0]
+        for dataset in map(Path, experiment[1:]):
+            with dataset.open("rb") as f:
+                experiments[name].append(list(loadAll(f)))
 
-    stats = OrderedDict([("on times", []), ("blink times", []), ("# of blinks", []),
-                         ("# of photons", []), ("photons/blink", []), ("photons/frame", [])])
-    for ds_rois, ds_traces in zip(rois, traces):
-        ds_stats = {k: [] for k in stats.keys()}
-        for roi, trace in zip(ds_rois, ds_traces):
-            on = (trace > args.threshold)
+    stats = defaultdict(lambda: defaultdict(list))
+    for name, datasets in experiments.items():
+        for dataset in datasets:
+            ds_stats = defaultdict(list)
+            for roi in dataset:
+                trace = amax(roi, axis=(1, 2))
+                on = (trace > args.threshold)
 
-            # FIXME: Use raw intensity or intensity/background?
-            background = mean(roi[~on])
-            signal = clip(roi - background, a_min=0, a_max=float('inf'))
-            blinks = groupWith(signal, on)
-            on_blinks = map(lambda x: x[1], filter(lambda x: x[0], blinks))
+                background = mean(roi[~on])
+                # FIXME: Use raw intensity or intensity/background?
+                signal = clip(roi - background, a_min=0, a_max=float('inf'))
+                blinks = groupWith(signal, on)
+                on_blinks = map(lambda x: x[1], filter(lambda x: x[0], blinks))
 
-            photons_by_blink = list(map(list, map(partial(map, asum), on_blinks)))
-            ds_stats["photons/frame"].extend(chain.from_iterable(photons_by_blink))
-            ds_stats["photons/blink"].extend(map(sum, photons_by_blink))
-            ds_stats["# of photons"].append(sum(map(sum, photons_by_blink)))
-            ds_stats["blink times"].extend(map(len, photons_by_blink))
-            ds_stats["on times"].append(sum(map(len, photons_by_blink)))
-            ds_stats["# of blinks"].append(len(photons_by_blink))
-        for k, v in ds_stats.items():
-            stats[k].append(v)
+                photons_by_blink = list(map(list, map(partial(map, asum), on_blinks)))
+                ds_stats["photons/frame"].extend(chain.from_iterable(photons_by_blink))
+                ds_stats["photons/blink"].extend(map(sum, photons_by_blink))
+                ds_stats["# of photons"].append(sum(map(sum, photons_by_blink)))
+                ds_stats["blink times"].extend(map(len, photons_by_blink))
+                ds_stats["on times"].append(sum(map(len, photons_by_blink)))
+                ds_stats["# of blinks"].append(len(photons_by_blink))
+
+            for stat, v in ds_stats.items():
+                stats[name][stat].append(v)
 
     fig = plt.figure(figsize=(8, 12))
 
     if args.output is not None:
-        with open("{}_stats_summary.csv".format(args.output), 'w', newline='') as f:
-            writer = csv.DictWriter(f, fieldnames=("name", "mean", "standard deviation"))
-            writer.writeheader()
-            for title, stat in stats.items():
+        for name, exp_stats in stats.items():
+            with open("{}_{}.csv".format(args.output, name), 'w', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=("name", "mean", "standard deviation"))
+                writer.writeheader()
+                for title, stat in sorted(exp_stats.items()):
+                    grand_mean = mean(list(chain.from_iterable(stat)))
+                    variation = std(list(map(mean, stat)))
+                    writer.writerow({"name": title, "mean": grand_mean,
+                                     "standard deviation": variation})
+    else:
+        for name, exp_stats in stats.items():
+            print(name)
+            for title, stat in sorted(exp_stats.items()):
                 grand_mean = mean(list(chain.from_iterable(stat)))
                 variation = std(list(map(mean, stat)))
-                writer.writerow({"name": title, "mean": grand_mean,
-                                 "standard deviation": variation})
-    else:
-        for title, stat in stats.items():
-            grand_mean = mean(list(chain.from_iterable(stat)))
-            variation = std(list(map(mean, stat)))
-            print("{}: μ = {}, σ = {}".format(title, grand_mean, variation))
+                print("{}: μ = {}, σ = {}".format(title, grand_mean, variation))
+            print()
 
-
-    axes = map(partial(fig.add_subplot, len(stats) // 2, 2),
-               range(1, len(stats) + 1))
-    for ax, (title, data_sets) in zip(axes, stats.items()):
-        data = list(chain.from_iterable(data_sets))
+    titles = ["photons/frame", "photons/blink", "# of photons", "blink times",
+              "on times", "# of blinks",]
+    axes = map(partial(fig.add_subplot, len(titles) // 2, 2),
+               range(1, len(titles) + 1))
+    for ax, title in zip(axes, titles):
         ax.set_title(title)
-        bound = percentile(data, 95)
+
+        data = []
+        for name in stats:
+            data.append(list(chain.from_iterable(stats[name][title])))
+
+        bound = percentile(list(chain.from_iterable(data)), 95)
         bins = linspace(0, bound, min(bound, 20))
-        ax.hist(data, bins)
+        ax.hist(data, bins, normed=True, label=stats)
+        ax.legend()
 
     if args.output is not None:
         fig.tight_layout()
-        fig.savefig("{}_stats.png".format(args.output))
+        fig.savefig("{}.png".format(args.output))
 
     if args.output is None:
         plt.show()
