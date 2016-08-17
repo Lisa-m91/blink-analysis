f86850717294dc99617eb8d27296504bb48edd73
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-08-17 11:36:35 +0100

    Fix smooth.py
    Use opening/closing instead of erosion/dilation. Gives correct result for
    >1D cases.

--- smooth.py
+++ smooth.py
@@ -5,14 +5,20 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import morphology
 
 def ball(r, ndim):
-    from math import ceil, pi
+    from math import ceil, pi, inf
     from operator import sub
     from functools import reduce, partial
 
     size = ceil(2*r)
     xs = np.ogrid[(slice(-size/2, size/2, (size * 1j)),) * ndim]
     xs = map(lambda x: np.power(x, 2), xs)
-    return np.sqrt(reduce(sub, xs, r ** 2).clip(0)) - r
+    ys = np.sqrt(reduce(sub, xs, r ** 2).clip(0))
+    return ys - ys.max(), ys > 0
+
+def smooth(a, radius, invert=False):
+    function = morphology.grey_closing if invert else morphology.grey_opening
+    structure, footprint = ball(radius, a.ndim)
+    return function(a, structure=structure, footprint=footprint)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -21,16 +27,12 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Background correct an image using the rolling-ball method")
     parser.add_argument("tif", type=TiffFile)
     parser.add_argument("outfile", type=TiffWriter)
-    parser.add_argument("radius", type=float)
-    parser.add_argument("--invert", action="store_true", type=bool)
+    parser.add_argument("--radius", type=float, required=True)
+    parser.add_argument("--invert", action="store_true")
 
     args = parser.parse_args()
     with args.tif:
-        data = args.tif.asarray()
-
-    structure = ball(args.radius, data.ndim)
-    function = morphology.grey_dilation if args.invert else morphology.grey_erosion
-    data = function(data, structure=structure) + args.radius
+        data = args.tif.asarray().astype('float32')
 
     with args.outfile:
-        args.outfile.save(data.astype('float32'))
+        args.outfile.save(smooth(data, args.radius, args.invert).astype('float32'))
