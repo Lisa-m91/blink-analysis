4de0dd17e23565996a8900165cf6b49cf8a0162c
Kai Wohlfahrt <kjw53@cam.ac.uk>
2016-08-25 14:33:35 +0100

    Remove plotting from fret_analysis

--- fret_analysis.py
+++ fret_analysis.py
@@ -76,78 +76,9 @@ def analyze(args):
     with args.outfile.open("wb") as f:
         dump(dict(stats), f)
 
-def plot(args):
-    if args.outdir is not None:
-        matplotlib.use('agg')
-    # Must be imported after backend is set
-    import matplotlib.pyplot as plt
-
-    stats = defaultdict(partial(defaultdict, list))
-    for stat_file in args.statfile:
-        with stat_file.open("rb") as f:
-            name = load(f)
-            dataset_stats = load(f)
-            for stat_name, vs in dataset_stats.items():
-                stats[name][stat_name].append(vs)
-
-    summaries = defaultdict(dict)
-    for exp_name, exp_stats in stats.items():
-        for stat_name, ds_stats in exp_stats.items():
-            means = list(map(mean, stats[exp_name][stat_name]))
-            summaries[exp_name][stat_name] = Summary(mean(means), std(means))
-            stats[exp_name][stat_name] = list(chain.from_iterable(ds_stats))
-
-    figs = {}
-    fig, axs = plt.subplots(2, 3, figsize=(8, 12))
-    figs["histograms"] = fig
-    for stat_name, ax in zip(stat_names, chain.from_iterable(axs)):
-        ax.set_title(stat_name)
-        exp_names = list(stats.keys())
-        data = [stats[exp_name][stat_name] for exp_name in exp_names]
-        bound = percentile(list(chain.from_iterable(data)), 95)
-        bins = linspace(0, bound, min(bound, 20))
-        ax.hist(data, bins, normed=True, label=exp_names)
-        ax.legend()
-
-    fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-    figs["survival"] = fig
-    ax.set_title("Survival Curve")
-    ax.set_ylabel("% Remaining")
-    ax.set_yscale("log")
-    ax.set_xlabel("Lifetime")
-    for experiment_name, stat in stats.items():
-        on_times = array(stat["total_times"]).reshape(-1)
-        xs = arange(0, on_times.max() + 1)
-        surviving = asum(reshape(on_times, (-1, 1)) >= reshape(xs, (1, -1)), axis=0)
-        ax.plot(surviving / len(on_times) * 100, label=experiment_name)
-    ax.legend()
-
-    if args.outdir is not None:
-        for name, stat_summaries in summaries.items():
-            with (args.outdir / "{}.csv".format(name)).open("w", newline='') as f:
-                writer = csv.DictWriter(f, fieldnames=("name", "mean", "standard deviation"))
-                writer.writeheader()
-                for stat, summary in stat_summaries.items():
-                    writer.writerow({"name": stat, "mean": summary.mean,
-                                     "standard_deviation": summary.std_dev})
-        for fig_name, fig in figs.items():
-            fig.tight_layout()
-            fig.savefig("{}.pdf".format(fig_name))
-    else:
-        for name, stat_summaries in summaries.items():
-            print("{} (N = {})".format(name, len(stats[name]['total_times'])))
-            for stat, summary in stat_summaries.items():
-                print("{}: μ = {}, σ = {}"
-                      .format(stat, *roundMean(summary.mean, summary.std_dev)))
-            print()
-        plt.show()
-
 if __name__ == "__main__":
     from argparse import ArgumentParser
     from pathlib import Path
-
-    import matplotlib
-    import csv
 
     parser = ArgumentParser(description="Analyze single-particle traces.")
     subparsers = parser.add_subparsers()
@@ -159,13 +90,6 @@ if __name__ == "__main__":
     parser_analyze.add_argument("--bin", type=int, default=1,
                                 help="Number of frames to bin.")
     parser_analyze.set_defaults(func=analyze)
-
-    parser_plot = subparsers.add_parser('plot', help="Plot analysis of generated statistics")
-    parser_plot.add_argument("statfile", type=Path, nargs='+',
-                             help="The directory where stats are stored")
-    parser_plot.add_argument("--outdir", type=Path,
-                             help="The directory to save the output files in")
-    parser_plot.set_defaults(func=plot)
 
     args = parser.parse_args()
     args.func(args)
